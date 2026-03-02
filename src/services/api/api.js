19c8f96b9ba98c3ea23beb3363c0b5b50b3ab978
RaydomDcruz <sourcrempie@gmail.com>
2026-03-02 14:59:40 +0800

    Before copyhing to backend-api

--- src/services/api/api.js
+++ src/services/api/api.js
@@ -8,6 +8,7 @@ const api = axios.create({
 api.interceptors.request.use((config) => {
   const authStore = useAuthStore()
 
+  // 🔥 IMPORTANT: token is a ref → use .value
   if (authStore.token) {
     config.headers.Authorization = `Bearer ${authStore.token}`
   }
